73843d38d5cf498e43ded9b96ae60d39c361bddf
Лидия <lida.kalinina.2017@inbox.ru>
2023-10-06 18:50:36 +0500

    Создала первую, функцию для проверки длины строки

---
+++ js/functions.js
@@ -0,0 +1,6 @@
+//Функция для проверки длины строки
+function checkStringLength (string, maxLength) {
+  return string.length <= maxLength;
+}
+//Проверка
+checkStringLength('проверяемая строка', 20);
