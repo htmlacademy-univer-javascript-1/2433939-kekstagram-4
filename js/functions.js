167814369c019a12c3ad25fe303be6cd27498d02
Лидия <lida.kalinina.2017@inbox.ru>
2023-11-05 23:55:53 +0500

    задание 5.2

--- js/functions.js
+++ js/functions.js
@@ -35,3 +35,25 @@ const findNumber = (input) => {
 
 //Проверка
 findNumber('2023 год');
+
+//Функция, которая принимает время начала и конца рабочего дня, а также время старта и продолжительность встречи в минутах и возвращает true, если встреча не выходит за рамки рабочего дня, и false, если выходит
+const convertToMinutes = (arr) => ((+arr[0] * 60) + +arr[1]);
+
+const checkTime = (start, end, startMeeting, duration) => {
+  const startTime = start.split(':');
+  const endTime = end.split(':');
+  const startMeetingTime = startMeeting.split(':');
+
+  const startMinutesTime = convertToMinutes(startTime);
+  const endMinutesTime = convertToMinutes(endTime);
+  const startMeetingMinutesTime  = convertToMinutes(startMeetingTime);
+
+  return startMeetingMinutesTime >= startMinutesTime && startMeetingMinutesTime + duration <= endMinutesTime;
+};
+
+//проверка
+checkTime('08:00', '17:30', '14:00', 90);
+checkTime('8:0', '10:0', '8:0', 120);
+checkTime('08:00', '14:30', '14:00', 90);
+checkTime('14:00', '17:30', '08:0', 90);
+checkTime('8:00', '17:30', '08:00', 900);
